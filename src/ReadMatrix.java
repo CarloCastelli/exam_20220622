5d868aca459815d100aa5d9a45a2090316f16358
Marcello <marcello.cescutti@hotmail.it>
2022-06-22 12:31:31 +0200

    Risoluzione ReadMatrix

--- src/ReadMatrix.java
+++ src/ReadMatrix.java
@@ -12,8 +12,33 @@
  */
 public class ReadMatrix {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-  }
+
+        String nuovaStringa = args[0];
+        nuovaStringa = nuovaStringa.replaceAll(",", "");
+        nuovaStringa = nuovaStringa.replaceAll("\\{", "");
+        nuovaStringa = nuovaStringa.replaceAll("\\}", "");
+
+        int separatore = 0;
+
+        char[][] matriceTrasformata = new char[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (separatore < nuovaStringa.length())
+                    matriceTrasformata[i][j] = nuovaStringa.charAt(separatore);
+                separatore++;
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+
+                System.out.print(matriceTrasformata[i][j] + ", ");
+            }
+            System.out.println("");
+        }
+    }
 
 }
