51aefc00c0dc315336bad7e22d8c0d893e17d298
Marcello <marcello.cescutti@hotmail.it>
2022-06-22 10:26:23 +0200

    Risoluzione SumMatrixValues

--- src/SumMatrixValues.java
+++ src/SumMatrixValues.java
@@ -11,8 +11,28 @@
  */
 public class SumMatrixValues {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-  }
+        int[][] matrice = {
+                {2, 4, 6},
+                {2, 4, 6},
+                {2, 4, 6}
+        };
+
+        int sommaRiga = 0;
+
+        for (int i = 0; i < matrice.length; i++) {
+
+
+
+            for (int j = 0; j < matrice[0].length; j++) {
+
+                sommaRiga += matrice[i][j];
+            }
+
+        }
+
+        System.out.println("La somma degli elementi della matrice e': " + sommaRiga);
+    }
 
 }
