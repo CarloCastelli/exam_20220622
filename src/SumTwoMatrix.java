37e13040d4ad3117002fe5e1797ff75f791d3156
Marcello <marcello.cescutti@hotmail.it>
2022-06-22 11:26:41 +0200

    Risoluzione SumTwoMatrix

--- src/SumTwoMatrix.java
+++ src/SumTwoMatrix.java
@@ -24,8 +24,40 @@
  */
 public class SumTwoMatrix {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-  }
 
+        int[][] primaMatrice = {
+                {1, 2, 3},
+                {1, 2, 3},
+                {1, 2, 3}
+        };
+
+        int[][] secondaMatrice = {
+                {1, 2, 3},
+                {1, 2, 3},
+                {1, 2, 3}
+        };
+
+        int[][] matriceRisultato = new int[3][3];
+
+
+        for (int i = 0; i < primaMatrice.length; i++) {
+            for (int j = 0; j < primaMatrice[i].length; j++) {
+                matriceRisultato[i][j] = primaMatrice[i][j] + secondaMatrice[i][j];
+            }
+
+        }
+
+
+        for (int i = 0; i < matriceRisultato.length; i++) {
+            for (int j = 0; j < matriceRisultato.length; j++) {
+                System.out.print(matriceRisultato[i][j] + " ");
+            }
+            System.out.println();
+
+        }
+
+
+    }
 }
